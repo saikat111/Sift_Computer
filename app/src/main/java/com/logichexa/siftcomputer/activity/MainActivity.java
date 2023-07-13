e739a8eaf6775e2541e95e1759a8f1c8e56eb055
saikat111 <s8856010@gmail.com>
2023-07-13 16:31:26 +0600

    update one final done

--- app/src/main/java/com/logichexa/siftcomputer/activity/MainActivity.java
+++ app/src/main/java/com/logichexa/siftcomputer/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.logichexa.siftcomputer.activity;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
@@ -8,16 +10,28 @@ import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 import com.google.android.material.navigation.NavigationView;
 import com.logichexa.siftcomputer.R;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
@@ -33,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private NavigationView navigationView;
+    private RewardedAd rewardedAd;
+    private final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +58,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         turist = findViewById(R.id.turist);
         number = findViewById(R.id.number);
         location = findViewById(R.id.location);
+        //ads
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+        RewardedAd.load(this, "ca-app-pub-9312766620009008/9194230366",
+                adRequest, new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error.
+                        Log.d(TAG, loadAdError.toString());
+                        rewardedAd = null;
+                    }
+
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd ad) {
+                        rewardedAd = ad;
+
+
+                    }
+                });
+
+
+
+
+
         //image
         ArrayList imageList = new ArrayList<SlideModel>();
         //imageList.add(new SlideModel(String.valueOf(R.drawable.one), ScaleTypes.FIT));
@@ -59,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         medical.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                countClick(1);
                 Intent intent = new Intent(getApplicationContext(),MedicalActivity.class);
                 startActivity(intent);
 
@@ -67,6 +113,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         turist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                countClick(1);
                 Intent intent = new Intent(getApplicationContext(),TruestActivity.class);
                 startActivity(intent);
             }
@@ -74,6 +121,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         number.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                countClick(1);
                 String phoneNumber = "+8801746325048";
                 Intent intent = new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse("tel:" + phoneNumber));
@@ -83,7 +131,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                countClick(1);
                 String latitude = "24.89989272206985";  // Latitude of the location
                 String longitude = "89.35703611800838";  // Longitude of the location
                 String strUri = "http://maps.google.com/maps?q=loc:" + latitude + "," + longitude + " (" + "Sifat Computer & Other's" + ")";
@@ -116,6 +164,49 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
+    private void countClick(int i) {
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_APPEND);
+        int a = sh.getInt("count", 0);
+        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+        myEdit.putInt("count", a + i);
+        myEdit.commit();
+        loadAds();
+        SharedPreferences sh1 = getSharedPreferences("MySharedPref", MODE_APPEND);
+        int a1 = sh.getInt("count", 0);
+        Toast.makeText(getApplicationContext(),"click" + a1,Toast.LENGTH_LONG).show();
+    }
+
+    private void loadAds() {
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_APPEND);
+        int a = sh.getInt("count", 0);
+        Log.d(TAG, "Ad was loaded.");
+        if(a >= 5){
+            if (rewardedAd != null) {
+                Activity activityContext = MainActivity.this;
+                rewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+                    @Override
+                    public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                        // Handle the reward.
+
+                        SharedPreferences sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+                        SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                        myEdit.putInt("count", 1);
+                        myEdit.commit();
+
+                        Log.d(TAG, "The user earned the reward.");
+                        int rewardAmount = rewardItem.getAmount();
+                        String rewardType = rewardItem.getType();
+                    }
+                });
+            } else {
+                Log.d(TAG, "The rewarded ad wasn't ready yet.");
+            }
+        }
+
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle the ActionBarDrawerToggle click events
@@ -132,13 +223,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         // Perform actions based on the selected item ID
         if(itemId == R.id.nav_item1){
+            countClick(1);
             showAdressialog();
         } else if (itemId == R.id.nav_item2) {
+            countClick(1);
             shownumberDialog();
         }else if (itemId == R.id.nav_item3) {
+            countClick(1);
             openFacebookPage();
         }
         else if (itemId == R.id.nav_item4) {
+            countClick(1);
             openWebPage();
         }
 
@@ -158,16 +253,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private void openFacebookPage() {
         String facebookUrl = "https://www.facebook.com/SifatComputerandothersBD"; // Replace with your Facebook page URL
 
-            // If the Facebook app is not installed, open with a web browser
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
-            startActivity(intent);
+        // If the Facebook app is not installed, open with a web browser
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(facebookUrl));
+        startActivity(intent);
 
     }
     private void showAdressialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("আমাদের ঠিকানা")
                 .setMessage("হোটেল মোমো ইনের দক্ষিণ পাশে, ইসলাম প্লাজা, ডোকান নং-২, নওদাপাড়া, বগুড়া সদর, বগুড়া ।"
-                + "মোবাইল নাম্বার " + "  01746325048" + " হোয়াটসঅ্যাপ নম্বর" + " 01717830070")
+                        + "মোবাইল নাম্বার " + "  01746325048" + " হোয়াটসঅ্যাপ নম্বর" + " 01717830070")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
